530c334e16b5092adc7d7bfee162938e1abd6aba
sperry94 <sepe4375@colorado.edu>
2016-05-03 18:08:38 -0600

    Sanitize url input

--- prototype/js/script.js
+++ prototype/js/script.js
@@ -1,27 +1,31 @@
 var colToAppend = 0;
+var urlRegex = /https:\/\/craftml.io\/[a-zA-Z0-9]{5}/
 
 $(document).ready(function() {
   $('#sub-button').click(function() {
-    $('#gallery').append(
-        '<div class="col s4">'
-        +  '<div class="card subscribed-card" data-url="' + $('#sub_url').val() + '">'
-        +     '<div class="modal-trigger card-image waves-effect waves-block waves-light" href="#codeModal">'
-        +     '<img src="img/gallery.png">'
-        +     '<span class="card-title red-text"><i class="material-icons small">error</i></span>'
-        +     '<div class="title">'
-        +       '<span class="modelName activator grey-text text-darken-4">Hull Example<i class="material-icons small right">more_vert</i></span>'
-        +       '<p class="user">by calebhsu</p>'
-        +     '</div>'
-        +    '</div>'
-        // +    '<div class="card-reveal">'
-        // +       '<span class="card-title grey-text text-darken-4"><i class="material-icons right close">close</i></span>'
-        // +       '<img class="code" src="img/code.png">'
-        // +     '</div>'
-        +   '</div>'
-        +'</div>'
+    if(urlRegex.test($('#sub_url').val()))
+    {
+        $('#gallery').append(
+            '<div class="col s4">'
+            +  '<div class="card subscribed-card" data-url="' + $('#sub_url').val() + '">'
+            +     '<div class="modal-trigger card-image waves-effect waves-block waves-light" href="#codeModal">'
+            +     '<img src="img/gallery.png">'
+            +     '<span class="card-title red-text"><i class="material-icons small">error</i></span>'
+            +     '<div class="title">'
+            +       '<span class="modelName activator grey-text text-darken-4">Hull Example<i class="material-icons small right">more_vert</i></span>'
+            +       '<p class="user">by calebhsu</p>'
+            +     '</div>'
+            +    '</div>'
+            // +    '<div class="card-reveal">'
+            // +       '<span class="card-title grey-text text-darken-4"><i class="material-icons right close">close</i></span>'
+            // +       '<img class="code" src="img/code.png">'
+            // +     '</div>'
+            +   '</div>'
+            +'</div>'
 
-        );
-    colToAppend = (colToAppend + 1) % 4;
+            );
+        colToAppend = (colToAppend + 1) % 4;
+    }
   });
   $('#subModalButton').click(function() {
     $('#subModal').openModal();
